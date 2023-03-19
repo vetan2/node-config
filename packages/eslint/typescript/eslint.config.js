665690607992d695c7632d4fc77e5b4963a72a70
vetan2 <vetan2@naver.com>
2023-03-19 18:23:48 +0900

    Release 0.1.0

--- packages/eslint/typescript/eslint.config.js
+++ packages/eslint/typescript/eslint.config.js
@@ -1,20 +1,25 @@
 module.exports = {
-  extends: [
-    "@vetan2/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:import/typescript",
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  rules: {
-    // @typescript-eslint
-    "@typescript-eslint/no-empty-interface": "off",
-    "@typescript-eslint/no-namespace": "off",
-    "@typescript-eslint/no-empty-function": "off",
-  },
-  settings: {
-    "import/resolver": {
-      typescript: {},
+  extends: ["@vetan2/base"],
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/typescript",
+      ],
+      parser: "@typescript-eslint/parser",
+      plugins: ["@typescript-eslint"],
+      rules: {
+        // @typescript-eslint
+        "@typescript-eslint/no-empty-interface": "off",
+        "@typescript-eslint/no-namespace": "off",
+        "@typescript-eslint/no-empty-function": "off",
+      },
+      settings: {
+        "import/resolver": {
+          typescript: {},
+        },
+      },
     },
-  },
+  ],
 }
